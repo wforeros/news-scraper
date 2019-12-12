83e1e8dc2667a594c4982a2aaee71bb9b6befa84
Wilson Forero <wilson_camilo@hotmail.com>
2019-12-12 18:25:20 -0500

    Codigo comentado

--- common.py
+++ common.py
@@ -2,9 +2,14 @@ import yaml
 
 __config = None
 
+# Cargamos la config
 def config():
+	# Para usar la configuración que declaramos al inicio como None
 	global __config
+	# Si no estuvo cargada la vamos a cargar
 	if not __config:
+		# Abriendo en modo de lectura
 		with open('config.yaml', mode='r') as f:
 			__config = yaml.load(f)
+	# Retornamos la configruación
 	return __config
